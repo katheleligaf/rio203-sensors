649a7ae928c6518618d9d42e271c5979c50ed2e0
Katharine Gaffney <katharine.gaffney@telecom-paris.fr>
2024-01-28 14:10:20 +0100

    version 1 id

--- esp32/esp32_websocket/webSocket_Ultrasound_ESP32.ino
+++ esp32/esp32_websocket/webSocket_Ultrasound_ESP32.ino
@@ -1,10 +1,10 @@
-#include <ArduinoWebsockets.h>
 #include <ArduinoJson.h>
-#include <Wire.h>
+#include <ArduinoWebsockets.h>
 #include <WiFiClientSecure.h>
+#include <Wire.h>
 //#include <WebSocketClient.h>
-#include <math.h>
 #include "secret.h"
+#include <math.h>
 
 using namespace websockets;
 
@@ -12,6 +12,7 @@ using namespace websockets;
 #define trigPin 4
 const int ledRedPin = 23;
 const int ledGreenPin = 19;
+int idPlace = -1;
 
 const String serverName = "parking-rio.rezel.net";
 char path[] = "/";
@@ -21,60 +22,62 @@ unsigned long timerDelay = 5000;
 
 const long soundSpeed = 343.2;
 long duration, distance;
+
 String previousState = "busy";
 String currentState = "free";
 
-const char ssl_cert[]  = \
-  "-----BEGIN CERTIFICATE-----\n" \
-  "MIIFYDCCBEigAwIBAgIQQAF3ITfU6UK47naqPGQKtzANBgkqhkiG9w0BAQsFADA/\n" \
-  "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
-  "DkRTVCBSb290IENBIFgzMB4XDTIxMDEyMDE5MTQwM1oXDTI0MDkzMDE4MTQwM1ow\n" \
-  "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-  "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwggIiMA0GCSqGSIb3DQEB\n" \
-  "AQUAA4ICDwAwggIKAoICAQCt6CRz9BQ385ueK1coHIe+3LffOJCMbjzmV6B493XC\n" \
-  "ov71am72AE8o295ohmxEk7axY/0UEmu/H9LqMZshftEzPLpI9d1537O4/xLxIZpL\n" \
-  "wYqGcWlKZmZsj348cL+tKSIG8+TA5oCu4kuPt5l+lAOf00eXfJlII1PoOK5PCm+D\n" \
-  "LtFJV4yAdLbaL9A4jXsDcCEbdfIwPPqPrt3aY6vrFk/CjhFLfs8L6P+1dy70sntK\n" \
-  "4EwSJQxwjQMpoOFTJOwT2e4ZvxCzSow/iaNhUd6shweU9GNx7C7ib1uYgeGJXDR5\n" \
-  "bHbvO5BieebbpJovJsXQEOEO3tkQjhb7t/eo98flAgeYjzYIlefiN5YNNnWe+w5y\n" \
-  "sR2bvAP5SQXYgd0FtCrWQemsAXaVCg/Y39W9Eh81LygXbNKYwagJZHduRze6zqxZ\n" \
-  "Xmidf3LWicUGQSk+WT7dJvUkyRGnWqNMQB9GoZm1pzpRboY7nn1ypxIFeFntPlF4\n" \
-  "FQsDj43QLwWyPntKHEtzBRL8xurgUBN8Q5N0s8p0544fAQjQMNRbcTa0B7rBMDBc\n" \
-  "SLeCO5imfWCKoqMpgsy6vYMEG6KDA0Gh1gXxG8K28Kh8hjtGqEgqiNx2mna/H2ql\n" \
-  "PRmP6zjzZN7IKw0KKP/32+IVQtQi0Cdd4Xn+GOdwiK1O5tmLOsbdJ1Fu/7xk9TND\n" \
-  "TwIDAQABo4IBRjCCAUIwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYw\n" \
-  "SwYIKwYBBQUHAQEEPzA9MDsGCCsGAQUFBzAChi9odHRwOi8vYXBwcy5pZGVudHJ1\n" \
-  "c3QuY29tL3Jvb3RzL2RzdHJvb3RjYXgzLnA3YzAfBgNVHSMEGDAWgBTEp7Gkeyxx\n" \
-  "+tvhS5B1/8QVYIWJEDBUBgNVHSAETTBLMAgGBmeBDAECATA/BgsrBgEEAYLfEwEB\n" \
-  "ATAwMC4GCCsGAQUFBwIBFiJodHRwOi8vY3BzLnJvb3QteDEubGV0c2VuY3J5cHQu\n" \
-  "b3JnMDwGA1UdHwQ1MDMwMaAvoC2GK2h0dHA6Ly9jcmwuaWRlbnRydXN0LmNvbS9E\n" \
-  "U1RST09UQ0FYM0NSTC5jcmwwHQYDVR0OBBYEFHm0WeZ7tuXkAXOACIjIGlj26Ztu\n" \
-  "MA0GCSqGSIb3DQEBCwUAA4IBAQAKcwBslm7/DlLQrt2M51oGrS+o44+/yQoDFVDC\n" \
-  "5WxCu2+b9LRPwkSICHXM6webFGJueN7sJ7o5XPWioW5WlHAQU7G75K/QosMrAdSW\n" \
-  "9MUgNTP52GE24HGNtLi1qoJFlcDyqSMo59ahy2cI2qBDLKobkx/J3vWraV0T9VuG\n" \
-  "WCLKTVXkcGdtwlfFRjlBz4pYg1htmf5X6DYO8A4jqv2Il9DjXA6USbW1FzXSLr9O\n" \
-  "he8Y4IWS6wY7bCkjCWDcRQJMEhg76fsO3txE+FiYruq9RUWhiF1myv4Q6W+CyBFC\n" \
-  "Dfvp7OOGAN6dEOM4+qR9sdjoSYKEBpsr6GtPAQw4dy753ec5\n" \
-  "-----END CERTIFICATE-----\n";
+const char ssl_cert[] =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIFYDCCBEigAwIBAgIQQAF3ITfU6UK47naqPGQKtzANBgkqhkiG9w0BAQsFADA/\n"
+    "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"
+    "DkRTVCBSb290IENBIFgzMB4XDTIxMDEyMDE5MTQwM1oXDTI0MDkzMDE4MTQwM1ow\n"
+    "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+    "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwggIiMA0GCSqGSIb3DQEB\n"
+    "AQUAA4ICDwAwggIKAoICAQCt6CRz9BQ385ueK1coHIe+3LffOJCMbjzmV6B493XC\n"
+    "ov71am72AE8o295ohmxEk7axY/0UEmu/H9LqMZshftEzPLpI9d1537O4/xLxIZpL\n"
+    "wYqGcWlKZmZsj348cL+tKSIG8+TA5oCu4kuPt5l+lAOf00eXfJlII1PoOK5PCm+D\n"
+    "LtFJV4yAdLbaL9A4jXsDcCEbdfIwPPqPrt3aY6vrFk/CjhFLfs8L6P+1dy70sntK\n"
+    "4EwSJQxwjQMpoOFTJOwT2e4ZvxCzSow/iaNhUd6shweU9GNx7C7ib1uYgeGJXDR5\n"
+    "bHbvO5BieebbpJovJsXQEOEO3tkQjhb7t/eo98flAgeYjzYIlefiN5YNNnWe+w5y\n"
+    "sR2bvAP5SQXYgd0FtCrWQemsAXaVCg/Y39W9Eh81LygXbNKYwagJZHduRze6zqxZ\n"
+    "Xmidf3LWicUGQSk+WT7dJvUkyRGnWqNMQB9GoZm1pzpRboY7nn1ypxIFeFntPlF4\n"
+    "FQsDj43QLwWyPntKHEtzBRL8xurgUBN8Q5N0s8p0544fAQjQMNRbcTa0B7rBMDBc\n"
+    "SLeCO5imfWCKoqMpgsy6vYMEG6KDA0Gh1gXxG8K28Kh8hjtGqEgqiNx2mna/H2ql\n"
+    "PRmP6zjzZN7IKw0KKP/32+IVQtQi0Cdd4Xn+GOdwiK1O5tmLOsbdJ1Fu/7xk9TND\n"
+    "TwIDAQABo4IBRjCCAUIwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYw\n"
+    "SwYIKwYBBQUHAQEEPzA9MDsGCCsGAQUFBzAChi9odHRwOi8vYXBwcy5pZGVudHJ1\n"
+    "c3QuY29tL3Jvb3RzL2RzdHJvb3RjYXgzLnA3YzAfBgNVHSMEGDAWgBTEp7Gkeyxx\n"
+    "+tvhS5B1/8QVYIWJEDBUBgNVHSAETTBLMAgGBmeBDAECATA/BgsrBgEEAYLfEwEB\n"
+    "ATAwMC4GCCsGAQUFBwIBFiJodHRwOi8vY3BzLnJvb3QteDEubGV0c2VuY3J5cHQu\n"
+    "b3JnMDwGA1UdHwQ1MDMwMaAvoC2GK2h0dHA6Ly9jcmwuaWRlbnRydXN0LmNvbS9E\n"
+    "U1RST09UQ0FYM0NSTC5jcmwwHQYDVR0OBBYEFHm0WeZ7tuXkAXOACIjIGlj26Ztu\n"
+    "MA0GCSqGSIb3DQEBCwUAA4IBAQAKcwBslm7/DlLQrt2M51oGrS+o44+/yQoDFVDC\n"
+    "5WxCu2+b9LRPwkSICHXM6webFGJueN7sJ7o5XPWioW5WlHAQU7G75K/QosMrAdSW\n"
+    "9MUgNTP52GE24HGNtLi1qoJFlcDyqSMo59ahy2cI2qBDLKobkx/J3vWraV0T9VuG\n"
+    "WCLKTVXkcGdtwlfFRjlBz4pYg1htmf5X6DYO8A4jqv2Il9DjXA6USbW1FzXSLr9O\n"
+    "he8Y4IWS6wY7bCkjCWDcRQJMEhg76fsO3txE+FiYruq9RUWhiF1myv4Q6W+CyBFC\n"
+    "Dfvp7OOGAN6dEOM4+qR9sdjoSYKEBpsr6GtPAQw4dy753ec5\n"
+    "-----END CERTIFICATE-----\n";
 
 WiFiClientSecure client;
 WebsocketsClient webSocketClient;
 
-String getMacAddress () {
-  byte mac[6]; // the MAC address of your 
+String getMacAddress() {
+  byte mac[6]; // the MAC address of your
   WiFi.macAddress(mac);
-  return String(mac[0], HEX) + ":" + String(mac[1], HEX) + ":" + String(mac[2], HEX) + ":" + String(mac[3], HEX) + ":" + String(mac[4], HEX) + ":" + String(mac[5], HEX);
+  return String(mac[0], HEX) + ":" + String(mac[1], HEX) + ":" +
+         String(mac[2], HEX) + ":" + String(mac[3], HEX) + ":" +
+         String(mac[4], HEX) + ":" + String(mac[5], HEX);
 }
 
-void sendJson(const JsonDocument doc) {
-  String out;
-  serializeJson(doc, out);
-  sendWebSocketRequest(out);
-}
-
-void sendWebSocketRequest(const String& request) {
+void sendJson(JsonDocument doc) {
+  if (idPlace != -1) {
+    doc["id"] = idPlace;
+  }
+  doc["name"] = getMacAddress();  String json_str;
+  serializeJson(doc, json_str);
   if (client.connected()) {
-    webSocketClient.send(request);
+    webSocketClient.send(json_str);
     Serial.println("Request sent.");
   }
 }
@@ -87,7 +90,7 @@ void setup() {
   pinMode(ledGreenPin, OUTPUT);
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -95,7 +98,7 @@ void setup() {
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
   delay(5000);
-  client.setInsecure();                                                                             // we live in a twilight world
+  client.setInsecure(); // we live in a twilight world
   Serial.println("Waiting for connection.");
   while (!client.connect(host, 443)) {
     delay(500);
@@ -113,56 +116,65 @@ void setup() {
 
   webSocketClient.onMessage([&](WebsocketsMessage message) {
     if (client.connected()) {
-        String data = message.data();
+      String data = message.data();
 
-        if (data.length() > 0) {
-            Serial.print("Received data: ");
-            Serial.println(data);
+      if (data.length() > 0) {
+        Serial.print("Received data: ");
+        Serial.println(data);
 
-            // Deserialize json
-            JsonDocument receivedObj;
-            deserializeJson(receivedObj, data);
+        // Deserialize json
+        JsonDocument receivedObj;
+        deserializeJson(receivedObj, data);
 
-            // Here answer to message
-            const char* request = receivedObj["request"];
-            JsonDocument toSendObj;
-            toSendObj["name"] = getMacAddress();
-            if (strcmp(request, "name") == 0) {
-                toSendObj["response"] = "name";
-                sendJson(toSendObj);
-            } else if (strcmp(request, "state") == 0) {
-                toSendObj["response"] = "state";
-                if (currentState != nullptr) {
-                    if (currentState == "busy" || currentState == "free" || currentState == "reserved")  {
-                        toSendObj["state"] = currentState;
-                    } else {
-                        toSendObj["state"] = "error";
-                    }
-                    
-                    }
-                } else if (strcmp(request, "setState") == 0) {
-                    currentState = "reserved";
-                    previousState = currentState;
-                    toSendObj["response"] = "setState";
-                    toSendObj["state"] = currentState;
-                    sendJson(toSendObj);
-                }  
-                  else {
-                    toSendObj["response"] = "error";
-                    sendJson(toSendObj);
-                }
-         }
-     }
-});
+        // Here answer to message
+        const char *request = receivedObj["request"];
+        JsonDocument toSendObj;
+        if (strcmp(request, "name") == 0) {
+          toSendObj["response"] = "name";
+          if (idPlace == -1) {
+            JsonDocument idRequestObj;
+            idRequestObj["request"] = "getId";
+            sendJson(idRequestObj);
+          }
+          sendJson(toSendObj);
+        } else if (strcmp(request, "getId") == 0) {
+          idPlace = receivedObj["id"];
+        } else if (strcmp(request, "setId") == 0) {
+          idPlace = receivedObj["id"];
+        } else if (strcmp(request, "state") == 0) {
+          toSendObj["response"] = "state";
+          if (currentState != nullptr) {
+            if (currentState == "busy" || currentState == "free" ||
+                currentState == "reserved") {
+              toSendObj["state"] = currentState;
+            } else {
+              toSendObj["state"] = "error";
+            }
+          }
+        } else if (strcmp(request, "setState") == 0) {
+          currentState = "reserved";
+          previousState = currentState;
+          toSendObj["response"] = "setState";
+          toSendObj["state"] = currentState;
+          sendJson(toSendObj);
+        } else {
+          toSendObj["response"] = "error";
+          sendJson(toSendObj);
+        }
+      }
+    }
+  });
 }
 
-
-void switchLed(const String& color) {
+void switchLed(const String &color) {
   if (color == "red") {
     analogWrite(ledGreenPin, LOW);
     analogWrite(ledRedPin, HIGH);
   } else if (color == "green") {
     digitalWrite(ledRedPin, LOW);
+    digitalWrite(ledGreenPin, HIGH);
+  } else if (color == "orange") {
+    digitalWrite(ledRedPin, HIGH);
     digitalWrite(ledGreenPin, HIGH);
   }
 }
@@ -193,18 +205,20 @@ void loop() {
   }
   
   if (currentState != previousState) {
-    if (currentState == "busy" || currentState == "reserved") {
+    if (currentState == "busy") {
       switchLed("red");
+    } else if (currentState == "reserved") {
+      switchLed("orange");
     } else {
       switchLed("green");
     }
+    
     JsonDocument toSendObj;
-    toSendObj["name"] = getMacAddress();
-    toSendObj["info"] = "state";
+    toSendObj["request"] = "info";
     toSendObj["state"] = currentState;
     previousState = currentState;
     sendJson(toSendObj);
-}
+  }
 
   // Reset the timer after sending the WebSocket request
   lastTime = millis();
